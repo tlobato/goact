328a807f570b2ee595805465817c3e1a67598dd2
Tomas Lobato <155578808+tlobato@users.noreply.github.com>
2025-01-01 18:50:52 -0300

    Deleted wrong dir

--- app/client/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-   <h1 className="text-2xl">Hello from Goact!</h1>
-  </StrictMode>,
-)
--- app/server/handlers.go
+++
@@ -1,9 +0,0 @@
-package server
-
-import (
-		"github.com/gofiber/fiber/v2"
-)
-
-func HandleHello(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{"message": "hello!"})
-}
